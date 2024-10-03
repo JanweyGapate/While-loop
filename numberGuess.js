e555b6777f91fb1cb00aacafd8fa1fffa1be55e7
JanweyGapate <janweygapate@gmail.com>
2024-10-03 23:27:14 +0800

    Create numberGuess.js

---
+++ numberGuess.js
@@ -0,0 +1,21 @@
+let secretNumber = Math.floor(Math.random() * 10) + 1;
+
+
+let guess =("2");
+
+
+guess = (guess)
+
+// Use a while loop to keep asking until the user guesses correctly
+while (guess !== secretNumber) {
+  
+  if (guess > secretNumber) 
+    {console.log("Too high!");} 
+  else 
+  {console.log("Too low!");}
+
+ 
+  guess = ("4");
+  guess = parseInt(guess);}
+
+console.log("Correct!");
